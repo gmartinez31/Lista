8ed1487fa3338ff1c7039e2daefe86d5abaf3487
gmartinez31 <bhgustavo13@yahoo.com>
2020-10-21 00:15:17 -0600

    Created Todo Entity

--- app/src/main/java/com/zoose/lista/todo/Todo.kt
+++ app/src/main/java/com/zoose/lista/todo/Todo.kt
@@ -1,9 +1,18 @@
 package com.zoose.lista.todo
 
-import androidx.ro
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+
+@Entity(tableName = "todo")
 data class Todo (
+    @PrimaryKey(autoGenerate = true)
     var id: Long = 0L,
+
+    @ColumnInfo(name = "complete")
     var complete: Boolean = false,
+
+    @ColumnInfo(name = "description")
     var description: String
 )
